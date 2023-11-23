09dd7c5c36457209f60a47afe2e7cbd1ef32cb86
mzunohkaru <winkaisyu@gmail.com>
2023-11-23 16:59:34 +0900

    second

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:freezed_sample/main.dart';
+import 'package:freezed_sample/JSON_PARSE/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
